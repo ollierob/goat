691946ecf3212c7d8bddd00f3f6eba7e068c407c
Ollie <ollie.robertshaw@gmail.com>
2016-06-04 13:45:02 +0100

    Fix test

--- goat-money/src/test/java/net/ollie/goat/money/DecimalMoneyTest.java
+++ goat-money/src/test/java/net/ollie/goat/money/DecimalMoneyTest.java
@@ -1,7 +1,6 @@
 package net.ollie.goat.money;
 
 import java.math.BigDecimal;
-import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -21,32 +20,17 @@ public class DecimalMoneyTest {
         final Currency mockCurrency = new Currency() {
 
             @Override
-            public String toString() {
-                return "ABC";
-            }
-
-            @Override
             public String symbol() {
                 throw new UnsupportedOperationException(); //TODO
             }
 
             @Override
             public String uniqueSymbol() {
-                throw new UnsupportedOperationException(); //TODO
-            }
-
-            @Override
-            public Currency currency() {
-                throw new UnsupportedOperationException(); //TODO
-            }
-
-            @Override
-            public Set<? extends Currency> currencies() {
-                throw new UnsupportedOperationException(); //TODO
+                return "DEF";
             }
 
         };
-        assertThat(new DecimalMoney(mockCurrency, BigDecimal.ONE).toString(), is("ABC 1"));
+        assertThat(new DecimalMoney(mockCurrency, BigDecimal.ONE).toString(), is("DEF 1"));
     }
 
 }
