c8b461e58665d46037a1115464f77d2d60b04985
Ollie <ollie.robertshaw@gmail.com>
2017-08-26 18:29:11 +0100

    require

--- data/src/main/java/net/ollie/goat/data/Provider.java
+++ data/src/main/java/net/ollie/goat/data/Provider.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
@@ -29,14 +29,16 @@ public interface Provider<K, V> {
         return Objects.requireNonNull(this.get(key), () -> "Missing [" + key + "]!");
     }
 
-    default <X extends Exception> V require(final K key, final Supplier<X> ifAbsent) throws X {
+    @Nonnull
+    default <X extends Exception> V require(final K key, final Function<? super K, ? extends X> ifAbsent) throws X {
         final V value = this.get(key);
         if (value == null) {
-            throw ifAbsent.get();
+            throw ifAbsent.apply(key);
         }
         return value;
     }
 
+    @Nonnull
     default Map<K, V> getAll(final Set<K> keys) {
         final Map<K, V> all = new HashMap<>(keys.size());
         for (final K key : keys) {
