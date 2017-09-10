99f44d2e514ea41698bf5be2295c650560b9577a
Ollie <ollie.robertshaw@gmail.com>
2017-09-10 12:05:54 +0100

    Date wrapper

--- dates/src/main/java/net/ollie/goat/temporal/date/DateWrapper.java
+++ dates/src/main/java/net/ollie/goat/temporal/date/DateWrapper.java
@@ -1,13 +1,19 @@
 package net.ollie.goat.temporal.date;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.chrono.ChronoLocalDate;
+import java.time.chrono.IsoChronology;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalField;
+import java.time.temporal.TemporalUnit;
 import java.util.Objects;
 
 /**
  *
  * @author Ollie
  */
-public abstract class DateWrapper implements HasDate {
+public abstract class DateWrapper implements HasDate, ChronoLocalDate {
 
     private final LocalDate date;
 
@@ -18,6 +24,31 @@ public abstract class DateWrapper implements HasDate {
     @Override
     public LocalDate date() {
         return date;
+    }
+
+    @Override
+    public IsoChronology getChronology() {
+        return date.getChronology();
+    }
+
+    @Override
+    public int lengthOfMonth() {
+        return date.lengthOfMonth();
+    }
+
+    @Override
+    public long until(Temporal endExclusive, TemporalUnit unit) {
+        return date.until(endExclusive, unit);
+    }
+
+    @Override
+    public Period until(final ChronoLocalDate endDateExclusive) {
+        return date.until(endDateExclusive);
+    }
+
+    @Override
+    public long getLong(final TemporalField field) {
+        return date.getLong(field);
     }
 
     @Override
